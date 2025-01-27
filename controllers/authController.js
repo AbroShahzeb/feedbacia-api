4cec62d33f368483ff36249a746bb65a6a63b15c
AbroShahzeb <khizr77@gmail.com>
2025-01-27 13:40:01 +0500

    Changes

--- controllers/authController.js
+++ controllers/authController.js
@@ -25,7 +25,7 @@ const createSendToken = (user, statusCode, res) => {
 
   if (process.env.NODE_ENV === "production") {
     cookieOptions.secure = true;
-    cookieOptions.domain = ".vercel.app";
+    // cookieOptions.domain = ".vercel.app";
   }
 
   res.cookie("jwt", token, cookieOptions);

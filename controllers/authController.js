a6573e7ac976f3089aea8ec0dbe2c9b5b1f22516
AbroShahzeb <khizr77@gmail.com>
2025-01-27 13:09:51 +0500

    Changes

--- controllers/authController.js
+++ controllers/authController.js
@@ -18,7 +18,7 @@ const createSendToken = (user, statusCode, res) => {
   const cookieOptions = {
     maxAge: process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    SameSite: "None",
+    sameSite: "None",
   };
 
   user.password = undefined;

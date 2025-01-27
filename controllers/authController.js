d5fe5e225adab52e3d789897590ed329c0f4d474
AbroShahzeb <khizr77@gmail.com>
2025-01-27 13:43:50 +0500

    Chnages

--- controllers/authController.js
+++ controllers/authController.js
@@ -18,15 +18,13 @@ const createSendToken = (user, statusCode, res) => {
   const cookieOptions = {
     maxAge: process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
     httpOnly: true,
-
   };
 
   user.password = undefined;
 
   if (process.env.NODE_ENV === "production") {
     cookieOptions.secure = true;
-    cookieOptions.sameSite =  "None",
-    // cookieOptions.domain = ".vercel.app";
+    cookieOptions.sameSite = "None";
   }
 
   res.cookie("jwt", token, cookieOptions);
